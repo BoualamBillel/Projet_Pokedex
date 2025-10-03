4f4a49fb9966a89fd7901472832feb9fb8467d61
Billel Boualam <boualambillelsofien@gmail.com>
2025-10-03 12:02:53 +0200

    save

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function getPokemonDatasById(id) {
         const response = await fetch(`https://pokebuildapi.fr/api/v1/${id}`);
         const pokemonDatasById = await response.json();
 
+        console.table(pokemonDatasById);
         return pokemonDatasById;
     } catch (error) {
         console.error("Erreur lors de la récupération des données du Pokémon par ID ou Nom");
@@ -57,7 +58,7 @@ function createPokemonListContainerCard(pokemonDatas) {
         const pokemonListContainer = document.createElement("div")
         const pokemonListTextInfosDiv = document.createElement("div");
         const pokemonListImgDiv = document.createElement("div");
-        
+
         // Ajout des classes aux divs
         pokemonListContainer.classList.add("pokemon-list-container");
         pokemonListTextInfosDiv.classList.add("pokemon-list-text-infos");
@@ -72,7 +73,7 @@ function createPokemonListContainerCard(pokemonDatas) {
         pokemonListId.innerText = pokemon.id;
         pokemonListName.innerText = pokemon.name
         pokemonListImg.setAttribute("src", pokemon.image);
-        
+
         // Insertion des élements dans leur div correspondants
         pokemonListTextInfosDiv.appendChild(pokemonListId);
         pokemonListTextInfosDiv.appendChild(pokemonListName);
@@ -88,3 +89,68 @@ function createPokemonListContainerCard(pokemonDatas) {
 
 }
 
+function createPokemonInfoCard(pokemonDatas) {
+    // Récupération des infos nécessaires
+    const pokemonId = pokemonDatas.id;
+    const pokemonImg = pokemonDatas.image;
+    const pokemonName = pokemonDatas.name;
+    const pokemonTypesImgs = getPokemonTypesImg(pokemonDatas);
+
+    // Récupération du container de pokemon info
+    const pokemonInfoContainer = document.querySelector(".pokemon-infos-container");
+
+    // Création des élements nécessaires
+    const pokemonIdText_element = document.createElement("h1");
+    const pokemonNameText_element = document.createElement("h1");
+    const pokemonImg_element = document.createElement("img");
+    const pokemonEvolution_element = createPokemonListContainerCard(pokemonDatas);
+
+    // Insertion des données dans les élements
+    pokemonIdText_element = pokemonId;
+    pokemonImg_element = pokemonImg;
+    pokemonNameText_element = pokemonName;
+
+    // Insertion dans le container
+    pokemonInfoContainer.appendChild(pokemonIdText_element);
+    pokemonInfoContainer.appendChild(pokemonNameText_element);
+    pokemonInfoContainer.appendChild(pokemonImg_element);
+    pokemonTypesImgs.forEach((img) => {
+        const pokemonTypeImg_element = document.createElement("img");
+        pokemonTypeImg_element.setAttribute("src", img);
+    });
+    pokemonInfoContainer.appendChild(pokemonEvolution_element);
+
+}
+
+function fetchUserSearchInput () {
+    // Récupération du formulaire
+    const form = document.querySelector("#search-bar");
+    // Ecoute de l'évenement submit sur le fomulaire de recherche de Pokémon
+    form.addEventListener("submit", (event) => {
+        event.preventDefault;
+        const formData = new FormData(form);
+        const searchValue = formData.get("search-input");
+        return searchValue;
+    })
+}
+
+function displayPokemonInfo (searchInput){
+    // Récupération des données du Pokemon
+    const pokemonDatas = getPokemonDatasById(searchInput);
+    // Récupération du formulaire
+    const searchPokemonForm = document.querySelector("#search-bar");
+    // Ecoute de l'évenement submit sur le formulaire pour afficher les infos du Pokémon
+    searchPokemonForm.addEventListener("submit", createPokemonInfoCard(pokemonDatas));
+}
+
+function main() {
+    const searchInput = fetchUserSearchInput();
+    console.log("input search = " + searchInput);
+    getPokemonDatasById(searchInput)
+    displayPokemonInfo(searchInput);
+}
+
+
+// EXEC DEBUG
+// EXEC
+main();
